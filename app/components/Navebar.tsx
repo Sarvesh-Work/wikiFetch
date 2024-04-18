038ebec77bc646df6e1aa861d041d571c7dc5d54
Sarvesh-Work <sarveshmunde10@gmail.com>
2024-04-18 15:24:30 +0530

    next js

--- app/components/Navebar.tsx
+++ app/components/Navebar.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
+import Search from "./Search";
 
 export default function Navebar() {
   return (
     <nav className="bg-slate-600 p-4 flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl">
       <h1 className=" text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
-        <Link href="/">WiKiRocket</Link>
+        <Link href="/">WiKiFetch</Link>
       </h1>
+      <Search/>
     </nav>
   );
 }
